9f018d13146715aee3e7905bee2b5a91a1054939
Rachel Wyatt <rw126@evansville.edu>
2018-07-30 11:05:11 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,6 +3,9 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def turn_count(board)
   counter = 0
   board.each do |space|
-  if space == "X" || "O"
-    counter += 1
+    if space != " "
+      counter += 1
+    else counter + 0
+    end
   end
+end
